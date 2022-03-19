b183c8d60c60d1de8d6d196feab3f87ae808eee7
Gavin <Gavinszczesniak@gmail.com>
2022-03-19 17:41:22 -0400

    Created a modal that displays your total cost. You can close the modal by clicking the background or close, and the user can press order when they want to pay. (Order does not work since this is only a front end project)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardhandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCardhandler} />
       <main>
         <Meals />
       </main>
