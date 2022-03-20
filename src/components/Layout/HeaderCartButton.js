5d4730b8a9540f2a6275391ecf1b9731c0ca89c8
Gavin <Gavinszczesniak@gmail.com>
2022-03-19 22:32:29 -0400

    Made the user able to add new orders or subtract orders when viewing order modal. New orders of same item now stacks on previous in the cart.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+
 import CardIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHightlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    setBtnIsHightlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHightlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
