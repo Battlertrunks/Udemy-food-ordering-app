bcc8d3bfdec04da3ce08fc4f1199421891fcf54a
Gavin <Gavinszczesniak@gmail.com>
2022-06-24 21:28:43 -0400

    Added checkout form to the modal and a error message if data does not load.

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -7,12 +7,18 @@ import classes from "./AvaliableMeals.module.css";
 const AvaliableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-d499f-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Http Error...");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,13 +35,27 @@ const AvaliableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals()
+      .then()
+      .catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+        console.log(error);
+      });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
